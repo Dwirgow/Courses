59bfd0ec3ced1579e9b88bc5d31d7a1477bbaade
Dwirgow <lucas.aulnoit@gmail.com>
2026-06-18 10:00:16 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'caddieclair-v1';
+const ASSETS_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+// Installation du Service Worker et mise en cache
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+// Activation
+self.addEventListener('activate', (event) => {
+  self.clients.claim();
+});
+
+// Interception des requêtes (Permet à l'application de fonctionner même avec une mauvaise connexion)
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
